a1ca4de27b7795350fa9571aa3fc9fe2a46d0d3c
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-30 16:48:12 -0400

    Section 3, Lecture 44-49: Course Project cont.

--- courseProject/src/app/app.module.ts
+++ courseProject/src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
-import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -17,11 +16,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    RecipeBookComponent,
-    RecipeItemComponent,
     RecipesComponent,
+    ShoppingListComponent,
     RecipeListComponent,
+    RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingEditComponent
   ],
