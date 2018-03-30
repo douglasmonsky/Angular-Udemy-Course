03e60bc9a69236828b672ade53d04f5ed8765f62
Douglas Monsky <douglas.monsky@gmail.com>
2018-03-30 15:44:22 -0400

    Section 3, Lecture 43: Component Setup

--- courseProject/src/app/app.module.ts
+++ courseProject/src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    RecipeItemComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
